7248af983a769627a6c550420d9e823bfa66bd3d
theeeeeng <theeeeeng@gmail.com>
2018-01-25 19:05:56 +0900

    firebase 호스팅 설정 추가

--- review-project/src/router/index.js
+++ review-project/src/router/index.js
@@ -4,9 +4,22 @@ import Home from '@/components/Home';
 
 import 'vue-awesome/icons';
 import Icon from 'vue-awesome/components/Icon';
+import VueFire from 'VueFire';
+import firebase from 'firebase';
 
 Vue.use(Router);
-Vue.component('icon', Icon);
+Vue.component(Icon);
+Vue.component(VueFire);
+
+var config = {
+    apiKey: "AIzaSyAel35QXr1X_OYA5pvDd-wX1dF5XzZ7HIY",
+    authDomain: "review-project-c5a79.firebaseapp.com",
+    databaseURL: "https://review-project-c5a79.firebaseio.com",
+    projectId: "review-project-c5a79",
+    storageBucket: "",
+    messagingSenderId: "606480748553"
+  };
+firebase.initializeApp(config);
 
 export default new Router({
   routes: [
